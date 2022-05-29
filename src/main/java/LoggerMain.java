471a0efc2157709c1bfdcac62dd126d59bdb9713
rohit <mailrg2004@gmail.com>
2022-05-29 21:24:54 +0530

    Initial commit

---
+++ src/main/java/LoggerMain.java
@@ -0,0 +1,22 @@
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Enumeration;
+
+public class LoggerMain {
+
+    Logger logger = LoggerFactory.getLogger(LoggerMain.class);
+
+    public void runLoggingTest() {
+        logger.debug("Logging debug test");
+        logger.info("Logging info test");
+        logger.error("Logging error test");
+        logger.warn("Logging warn test");
+    }
+
+    public static void main(String[] args) {
+        LoggerMain logTest = new LoggerMain();
+        logTest.runLoggingTest();
+    }
+}
